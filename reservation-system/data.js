fda71a0a49efaa78f9c8ec4a2a4d46cde501ccc3
Carlos Alleng <carlosalleng@Carloss-MacBook-Pro.local>
2024-10-26 14:53:34 -0400

    Created reservation-system App

---
+++ reservation-system/data.js
@@ -0,0 +1,21 @@
+module.exports = function () {
+  return {
+  reservations: [
+  { id: 1, area: "Conservation Area 1", timeSlot: "9:00 AM - 12:00 PM", price: 50 },
+  { id: 2, area: "Conservation Area 1", timeSlot: "12:00 PM - 3:00 PM", price: 50 },
+  { id: 3, area: "Conservation Area 1", timeSlot: "3:00 PM - 6:00 PM", price: 50 },
+  { id: 4, area: "Conservation Area 2", timeSlot: "9:00 AM - 12:00 PM", price: 50 },
+  { id: 5, area: "Conservation Area 2", timeSlot: "12:00 PM - 3:00 PM", price: 50 },
+  { id: 6, area: "Conservation Area 2", timeSlot: "3:00 PM - 6:00 PM", price: 50 },
+  { id: 7, area: "Conservation Area 3", timeSlot: "9:00 AM - 12:00 PM", price: 50 },
+  { id: 8, area: "Conservation Area 3", timeSlot: "12:00 PM - 3:00 PM", price: 50 },
+  { id: 9, area: "Conservation Area 3", timeSlot: "3:00 PM - 6:00 PM", price: 50 },
+  { id: 10, area: "Conservation Area 4", timeSlot: "9:00 AM - 12:00 PM", price: 50 },
+  { id: 11, area: "Conservation Area 4", timeSlot: "12:00 PM - 3:00 PM", price: 50 },
+  { id: 12, area: "Conservation Area 4", timeSlot: "3:00 PM - 6:00 PM", price: 50 }
+],
+orders: []
+};
+};
+
+
